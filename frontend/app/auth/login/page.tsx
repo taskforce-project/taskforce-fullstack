f1194fb3abd27e1126318736b630ce6a0d1d52b6
Miche1-Pierre <pierre.michel.work@gmail.com>
2026-05-16 11:45:34 -0400

    feat: simplify login and registration page layouts by removing unnecessary wrappers and adjusting styles

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -3,9 +3,5 @@
 import { LoginForm } from "@/components/auth/login/login-form";
 
 export default function LoginPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 w-full max-w-5xl mx-auto">
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />;
 }
--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -3,9 +3,5 @@
 import { SignupForm } from "@/components/auth/register/register-info-form";
 
 export default function RegisterPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 w-full max-w-6xl mx-auto">
-      <SignupForm />
-    </div>
-  );
+  return <SignupForm />;
 }
