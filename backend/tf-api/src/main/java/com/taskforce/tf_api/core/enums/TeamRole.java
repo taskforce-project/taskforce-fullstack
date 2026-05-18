f9bac8f735e9be52c33ca044cff442fd54831cc6
Miche1-Pierre <pierre.michel.work@gmail.com>
2026-05-18 19:22:25 -0400

    feat: add TeamRole enum to define member roles within a team

---
+++ backend/tf-api/src/main/java/com/taskforce/tf_api/core/enums/TeamRole.java
@@ -0,0 +1,11 @@
+package com.taskforce.tf_api.core.enums;
+
+/**
+ * Rôle d'un membre dans une team
+ */
+public enum TeamRole {
+    /** Responsable de l'équipe */
+    LEAD,
+    /** Membre standard */
+    MEMBER
+}
