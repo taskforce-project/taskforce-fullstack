ad5c4ff6bddffaa9164be69fbfda275080f0d9d8
Miche1-Pierre <pierre.michel.work@gmail.com>
2026-06-09 21:58:16 -0400

    feat: Refactor imports in profile-service to align with project structure

--- frontend/lib/api/profile-service.ts
+++ frontend/lib/api/profile-service.ts
@@ -1,4 +1,4 @@
-import apiClient from "./api-client";
+import { apiClient } from "./client";
 import { PROFILE_ROUTES } from "../config/api-routes";
 
 // ---------------------------------------------------------------------------
